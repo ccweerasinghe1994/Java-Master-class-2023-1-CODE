a5edcecec3dd1dc155d95792bca0730de1266f51
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-15 17:06:47 +0530

    abstract

---
+++ Abstract/src/Animal.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+abstract public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("Unknown animal sound!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
---
+++ Abstract/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Animal cat = new Cat("Misty");
+        Animal dog = new Dog("Rex");
+        Dog dog2 = new Dog("Rex");
+        Cat cat2 = new Cat("Misty");
+    }
+}
