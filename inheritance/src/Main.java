64667866ae2b4b7a1a45262fe6104341ded5d2df
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-15 15:01:42 +0530

    adding getters and setters in Programmer.java and creating Programmer instance in Main.java

--- inheritance/src/Main.java
+++ inheritance/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        String[] languages = {"Java", "C++", "Python"};
+        Programmer programmer = new Programmer("John", "123 Main St", "Senior", 30, languages);
 
+        System.out.println(programmer.getName());
+        System.out.println(programmer.getAddress());
+        System.out.println(programmer.getExperience());
+        System.out.println(programmer.getAge());
+        System.out.println(Arrays.toString(programmer.getProgrammingLanguages()));
+        programmer.writingCode();
+        System.out.println(  programmer.toString());
+        System.out.println(programmer.hashCode());
     }
 }
--- inheritance/src/Programmer.java
+++ inheritance/src/Programmer.java
@@ -4,6 +4,19 @@ public class Programmer extends Employee{
         super(name, address, experience, age);
     }
 
+    public Programmer(String name, String address, String experience, int age, String[] programmingLanguages) {
+        super(name, address, experience, age);
+        this.programmingLanguages = programmingLanguages;
+    }
+
+    public String[] getProgrammingLanguages() {
+        return programmingLanguages;
+    }
+
+    public void setProgrammingLanguages(String[] programmingLanguages) {
+        this.programmingLanguages = programmingLanguages;
+    }
+
     public void writingCode() {
         System.out.println("Writing code...");
     }
