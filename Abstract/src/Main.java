1c290f41da632de2fb75144af0bfe450bc26fd37
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-16 09:57:01 +0530

    initial setup for polymophism

--- Abstract/src/Main.java
+++ Abstract/src/Main.java
@@ -1,12 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        NumberExtractor numberExtractor = new NumberExtractor();
-        EmailExtractorReport emailExtractorReport = new EmailExtractorReport();
+        ExtractorReport numberExtractor = new NumberExtractor();
+        ExtractorReport emailExtractorReport = new EmailExtractorReport();
+
+        ExtractorReport[] extractorReports = {
+                numberExtractor,
+                emailExtractorReport
+        };
+
         try {
-            emailExtractorReport.prepareAndSendReport("src/data.txt");
+            for (ExtractorReport extractorReport : extractorReports) {
+                extractorReport.prepareAndSendReport("src/data.txt");
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println(1 + 1);
+        System.out.println("1" + "1");
     }
 }
