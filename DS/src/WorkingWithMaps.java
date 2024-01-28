9e2e6b9c5fa200a9108448f1c621fb677754998a
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-28 20:04:52 +0530

    Add Person class and update WorkingWithMaps.java

--- DS/src/WorkingWithMaps.java
+++ DS/src/WorkingWithMaps.java
@@ -1,8 +1,17 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WorkingWithMaps {
     public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("John", 20), new Diamond("blue"));
+        System.out.println(new Person("John", 20).hashCode());
+        System.out.println(new Person("John", 20).hashCode());
+//        System.out.println(map.get(new Person("John", 20)).hashCode());
+    }
+
+    private static void map() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("John", 20));
         map.put(2, new Person("Mary", 30));
@@ -23,6 +32,42 @@ public class WorkingWithMaps {
         System.out.println(map.values());
     }
 
-    static record Person(String name, int age) {
+    static class Person {
+        String name;
+        int age;
+
+        public Person() {
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return age == person.age && Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
     }
+
+    record Diamond(String name) {
+    }
+
+
 }
