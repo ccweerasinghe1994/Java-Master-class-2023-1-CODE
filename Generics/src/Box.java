0e73c876072f32c0fbfd5f172cbf98b873833bc0
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-29 19:15:40 +0530

    Refactor Box class to use generics

--- Generics/src/Box.java
+++ Generics/src/Box.java
@@ -1,3 +1,11 @@
-public class GenericClasses {
-    
+public class Box<T> {
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
 }
