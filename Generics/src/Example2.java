63291a5bee14b7927536774a19fd7a0369a6686d
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-29 19:05:14 +0530

    Add Example2 class with main method

---
+++ Generics/src/Example2.java
@@ -0,0 +1,8 @@
+public class Example2 {
+    public static void main(String[] args) {
+     Comparable<Integer> number = 10;
+        int i = number.compareTo(110);
+        System.out.println(i);
+
+    }
+}
