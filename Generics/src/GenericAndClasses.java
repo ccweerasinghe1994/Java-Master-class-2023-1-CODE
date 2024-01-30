1b5704d54de9d22fed1e0038c3134af14a069d1a
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-30 09:44:43 +0530

    Add examples of using generics in main method

--- Generics/src/GenericAndClasses.java
+++ Generics/src/GenericAndClasses.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class GenericAndClasses {
+public class GenericAndClasses {
+    public static void main(String[] args) {
+        Box<Phone> box = new Box<>();
+        Phone phone = new Phone("Samsung");
+        box.setT(phone);
+        System.out.println(box.getT());
+
+        Letter letter = new Letter("Hello");
+        Box<Letter> box2 = new Box<>();
+        box2.setT(letter);
+        System.out.println(box2.getT());
+    }
 }
