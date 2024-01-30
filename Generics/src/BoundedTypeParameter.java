ff8109c921b366a550a3416f0b194627a67aa122
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-30 20:39:09 +0530

    Update BoundedTypeParameter.java

--- Generics/src/BoundedTypeParameter.java
+++ Generics/src/BoundedTypeParameter.java
@@ -3,11 +3,12 @@ public class BoundedTypeParameter {
         System.out.println(countGreaterThan(new Integer[]{1, 2, 3, 4, 5}, 3) );
         System.out.println(countGreaterThan(new Double[]{1.1, 2.2, 3.3, 4.4, 5.5}, 3.3) );
         System.out.println(countGreaterThan(new String[]{"a", "b", "c", "d", "e"}, "c") );
-        
+
 
     }
-
-    static <T extends Comparable<T>> int countGreaterThan(T[] anArray, T elem) {
+    interface A {}
+    interface B {}
+    static <T extends Number & Comparable<T> & A & B> int countGreaterThan(T[] anArray, T elem) {
         int count = 0;
         for (T e : anArray) {
             if (e.compareTo(elem) > 0) {
