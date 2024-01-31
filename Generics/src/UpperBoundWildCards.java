bc11af28ad23b8733acf060c659a773dc1da2141
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-31 07:33:05 +0530

    Refactor print method to accept lists of numbers

--- Generics/src/UpperBoundWildCards.java
+++ Generics/src/UpperBoundWildCards.java
@@ -3,18 +3,20 @@ import java.util.List;
 
 public class UpperBoundWildCards {
     public static void main(String[] args) {
-        List<Object> list = List.of(1, 2, 3, 4, 5,"6",new Date());
+        // List<Object> list = List.of(1, 2, 3, 4, 5,"6",new Date());
         List<Integer> list2 = List.of(1, 2, 3, 4, 5);
         List<Double> list3 = List.of(1.0, 2.0, 3.0, 4.0, 5.0);
-        List<String> list4 = List.of("1", "2", "3", "4", "5");
+        // List<String> list4 = List.of("1", "2", "3", "4", "5");
+        List<Number> list5 = List.of();
 
-        print(list);
-        print(list2);
-        print(list3);
-        print(list4);
+        // print(list);
+        printNumbers(list2);
+        printNumbers(list3);
+        // print(list4);
+        printNumbers(list5);
     }
 
-    static void print(List<?> list){
+    static void printNumbers(List<? extends Number> list){
         list.forEach(System.out::println);
     }
 }
