3f4ad58af8ee91a52670c15dff8385d55e81f183
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-31 07:22:00 +0530

    Commented out unused code in BoundedTypeParameter.java

--- Generics/src/BoundedTypeParameter.java
+++ Generics/src/BoundedTypeParameter.java
@@ -1,8 +1,8 @@
 public class BoundedTypeParameter {
     public static void main(String[] args) {
-        System.out.println(countGreaterThan(new Integer[]{1, 2, 3, 4, 5}, 3) );
-        System.out.println(countGreaterThan(new Double[]{1.1, 2.2, 3.3, 4.4, 5.5}, 3.3) );
-        System.out.println(countGreaterThan(new String[]{"a", "b", "c", "d", "e"}, "c") );
+//        System.out.println(countGreaterThan(new Integer[]{1, 2, 3, 4, 5}, 3) );
+//        System.out.println(countGreaterThan(new Double[]{1.1, 2.2, 3.3, 4.4, 5.5}, 3.3) );
+//        System.out.println(countGreaterThan(new String[]{"a", "b", "c", "d", "e"}, "c") );
 
 
     }
