c9069bb018dca9cc2b2e14d461016ff538380549
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-01-31 08:54:55 +0530

    Add stream examples to WorkingWithStreams class

--- Streams/src/WorkingWithStreams.java
+++ Streams/src/WorkingWithStreams.java
@@ -1,2 +1,27 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.lang.String;
+
 public class WorkingWithStreams {
+
+    @Test
+    public void streams() {
+        List<String> names = List.of("John", "Paul", "George", "Ringo");
+        Stream<String> stringStream = names.stream();
+//        same as above
+        Stream<String> stringStream2 = Stream.of("John", "Paul", "George", "Ringo");
+
+        List<Object> objects = stringStream.limit(2).sorted(null).dropWhile(null)
+                .collect(Collectors.toList());
+
+        String[] strings = {"John", "Paul", "George", "Ringo"};
+//        How to use streams with arrays
+        Arrays.stream(strings).forEach(System.out::println);
+
+    }
+
 }
