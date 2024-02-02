d1685d5c98cf746621224f6fbb572777bf6922d9
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-02 15:04:29 +0530

    Add test methods to MyFirstTest class

--- maven/src/test/java/org/example/MyFirstTest.java
+++ maven/src/test/java/org/example/MyFirstTest.java
@@ -1,2 +1,16 @@
-package org.example;public class MyFirstTest {
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+    @Test
+    void MyFirstTest() {
+        System.out.println("Hello, World!");
+    }
+    @Test
+    void MySecondTest() {
+        System.out.println("Hello, World!");
+    }
+
+
 }
